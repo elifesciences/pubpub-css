7432e78aeb58eaf2cf3ddf2e60344abee19d030f
Gabriel Stein <g@gabestein.com>
2023-01-23 18:38:59 -0500

    use nav-bar-component in js nav

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body");
 const div = document.createElement("div");
-div.setAttribute("class", "elife-nav-menu");
+div.setAttribute("class", "elife-nav-menu nav-bar-component");
 
 const logo = document
   .querySelector(".nav-list > :first-child a")
