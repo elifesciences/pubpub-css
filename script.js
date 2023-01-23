4ad5c07004d9fbe9706fe8707a6645b46c77df5a
Gabriel Stein <g@gabestein.com>
2023-01-23 18:46:08 -0500

    style nav bar list and create ul

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const body = document.querySelector("body");
-const div = document.createElement("div");
-div.setAttribute("class", "elife-nav-menu nav-bar-component");
+// create menu div
+const menuDiv = document.createElement("div");
+menuDiv.setAttribute("class", "elife-nav-menu nav-bar-component");
 
+// clone logo and add to div
 const logo = document
   .querySelector(".nav-list > :first-child a")
   .cloneNode(true);
 
-div.appendChild(logo);
+menuDiv.appendChild(logo);
 
+const navItems = document.createElement("ul");
+navItems.setAttribute("class", "elife-nav-items");
+
+// select & move extra nav items to menu
 const researchCategories = document.querySelector(
   ".nav-bar-component ul.nav-list > :nth-child(7)"
 );
@@ -20,9 +25,12 @@ const authorGuide = document.querySelector(
 const reviewerGuide = document.querySelector(
   ".nav-bar-component ul.nav-list > :nth-child(10)"
 );
-div.appendChild(researchCategories);
-div.appendChild(insideElife);
-div.appendChild(authorGuide);
-div.appendChild(reviewerGuide);
 
+navItems.appendChild(researchCategories);
+navItems.appendChild(insideElife);
+navItems.appendChild(authorGuide);
+navItems.appendChild(reviewerGuide);
+
+// prepend menu div to body
+const body = document.querySelector("body");
 body.prepend(div);
