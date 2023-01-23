958279913e86f072cf337a2188e81b81f10ae8cf
Gabriel Stein <g@gabestein.com>
2023-01-23 18:53:02 -0500

    add classname and styling to menu logo

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ menuDiv.setAttribute("class", "elife-nav-menu nav-bar-component");
 // clone logo and add to div
 const logo = document
   .querySelector(".nav-list > :first-child a")
-  .cloneNode(true);
+  .cloneNode(true)
+  .setAttribute("class", "elife-nav-logo");
 
 menuDiv.appendChild(logo);
 
