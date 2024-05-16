21b3e8d03d2b525f6e35009d1e6de8b1025e4d5d
Dave <discodavey@gmail.com>
2024-05-16 15:18:18 +0100

    Add cookiebot banner

--- script.js
+++ script.js
@@ -112,3 +112,12 @@ closeButton.addEventListener("click", toggleMenu);
 
 window.addEventListener('load', sizeMenuToPage);
 window.addEventListener('resize', sizeMenuToPage);
+
+let head = document.getElementsByTagName('head').item(0);
+const script = document.createElement('script');
+script.setAttribute('id', 'Cookiebot');
+script.setAttribute('type', 'text/javascript');
+script.setAttribute('src', 'https://consent.cookiebot.com/uc.js');
+script.setAttribute('data-cbid', '0a5c50d8-fcf9-47b1-8f4f-1eaadb13941b');
+script.setAttribute('data-blockingmode', 'auto');
+head.appendChild(script);s
